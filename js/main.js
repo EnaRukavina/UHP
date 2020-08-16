abc44470b414af649b073f69ff0b554ae140e134
Miloš Rakočević <milosgcm@gmail.com>
2020-08-16 16:00:24 +0200

    slider - update slider scroll

--- js/main.js
+++ js/main.js
@@ -4,4 +4,23 @@ $(document).ready(function () {
     $(".dots span").removeClass("active");
     $(this).addClass("active");
   });
+
+  var first_slide = $("#slider_images").find("img:first-child");
+  var second_slide = $("#slider_images").find("img:nth-child(2)");
+  var third_slide = $("#slider_images").find("img:nth-child(3)");
+  var fourth_slide = $("#slider_images").find("img:nth-child(4)");
+  var last_slide = $("#slider_images").find("img:last-child");
+
+  // Clone the last slide and add as first li element
+  last_slide.clone().prependTo($("#slider_images"));
+  second_slide.clone().prependTo($("#slider_images"));
+  third_slide.clone().prependTo($("#slider_images"));
+  fourth_slide.clone().prependTo($("#slider_images"));
+
+  // Clone the first slide and add as last li element
+  first_slide.clone().appendTo($("#slider_images"));
+
+  second_slide.clone().appendTo($("#slider_images"));
+  third_slide.clone().appendTo($("#slider_images"));
+  fourth_slide.clone().appendTo($("#slider_images"));
 });
